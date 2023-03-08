7761fd875e59e19e57fbe49dff1120aa00069a59
alex <alex@MacBook-Air-Mac.local>
2023-03-08 17:13:16 +0200

    Configure absolute paths and add nodemon

--- backend/src/tag/tag.controller.ts
+++ backend/src/tag/tag.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { TagService } from './tag.service';
+import { TagService } from '@app/tag/tag.service';
 
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
   @Get()
-  findAll() {
+  findAll(): string[] {
     return this.tagService.findAll();
   }
 }
