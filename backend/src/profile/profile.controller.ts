8ec7fc7b6afc629c8423579bef5fc92f5a78f7ad
alex <alex@MacBook-Air-Mac.local>
2023-03-23 22:32:27 +0200

    Add follow method

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -1,5 +1,6 @@
 import { User } from '@app/user/decorators/user.decorator';
-import { Controller, Get, Param } from '@nestjs/common';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
 
@@ -12,6 +13,19 @@ export class ProfileController {
     @Param('username') profileUsername: string,
   ): Promise<ProfileResponseInterface> {
     const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followProfile(
       currentUserId,
       profileUsername,
     );
